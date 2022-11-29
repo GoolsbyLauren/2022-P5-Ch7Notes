1fe3f58cd1c954a30fee4e19470e08b8f23492c6
LaurenG <lauren.goolsby@vtsd.com>
2022-11-29 13:12:03 -0500

    11/29

--- VoidReturn.java
+++ VoidReturn.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 public class VoidReturn {
-
    public static void main(String args[]){
-       
+      int total = addNums();
+
+      System.out.println("You total was: " + total);
+   }
+
+   public static int addNums(){
+      System.out.println("We are going to add 2 numbers.");
+      System.out.println("Please enter them below.");
+
+      Scanner scan = new Scanner(System.in);
+      int num1 = scan.nextInt();
+      int num2 = scan.nextInt();
+
+
+      return (num1 + num2);
 
    }
 
